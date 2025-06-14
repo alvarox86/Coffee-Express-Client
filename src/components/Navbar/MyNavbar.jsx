04d26b0da4636e8d20b6420959c4f054e4d18347
María <xmaryjs99@gmail.com>
2025-06-14 13:33:58 +0200

    cambios en el navbar y página de homePage creada

--- src/components/Navbar/MyNavbar.jsx
+++ src/components/Navbar/MyNavbar.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
-import "./MyNavBar.css"
-import cafeicon from "../../assets/images/cafeicon.png"
+import "./MyNavBar.css";
+import cafeicon from "../../assets/images/cafeicon.png";
 import axios from "axios";
 
 import AppBar from "@mui/material/AppBar";
@@ -19,20 +19,19 @@ import ListItem from "@mui/material/ListItem";
 import HomeIcon from "@mui/icons-material/Home";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InfoIcon from "@mui/icons-material/Info";
-import Face2Icon from '@mui/icons-material/Face2';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
+import Face2Icon from "@mui/icons-material/Face2";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function MyNavBar() {
-  const { isLoggedIn } = useContext(AuthContext)
-  const {loggedUserId} = useContext(AuthContext)
+  const { isLoggedIn } = useContext(AuthContext);
+  const { loggedUserId } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const { authenticateUser } = useContext(AuthContext)
+  const { authenticateUser } = useContext(AuthContext);
 
   const [open, setOpen] = React.useState(false);
 
@@ -41,87 +40,87 @@ function MyNavBar() {
   };
 
   const handleLogout = async () => {
-
     // borrar el token de local storage
-    localStorage.removeItem("authToken")
+    localStorage.removeItem("authToken");
 
     try {
-      
       // como el token no existe, la funcion cambia los estados del contexto para indicar que el usuario ya no está logeado
-      await authenticateUser()
+      await authenticateUser();
 
-      navigate("/") // o cualquier otra página pública
-
+      navigate("/"); // o cualquier otra página pública
     } catch (error) {
-      console.log(error) 
+      console.log(error);
     }
-  }
+  };
 
   //--------------------------------------------
 
-  const [userProfilePicture, setUserProfilePicture ] = useState(null)
-  const [userUserName, setUserUserName] = useState(null)
+  const [userProfilePicture, setUserProfilePicture] = useState(null);
+  const [userUserName, setUserUserName] = useState(null);
 
-  const params = useParams()
+  const params = useParams();
 
-  useEffect(() =>{
-    getData()
-  },[params])
+  useEffect(() => {
+    getData();
+  }, [params]);
 
-  const getData = async () =>{
-    const storedToken = localStorage.getItem("authToken")
+  const getData = async () => {
+    const storedToken = localStorage.getItem("authToken");
     try {
-      if(AuthContext){
-        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/user/${loggedUserId}`, { headers: { Authorization: `Bearer ${storedToken}` }})
+      if (AuthContext) {
+        const response = await axios.get(
+          `${import.meta.env.VITE_SERVER_URL}/api/user/${loggedUserId}`,
+          { headers: { Authorization: `Bearer ${storedToken}` } }
+        );
 
-        setUserUserName(response.data.username)
-        setUserProfilePicture(response.data.profilepicture)
+        setUserUserName(response.data.username);
+        setUserProfilePicture(response.data.profilepicture);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   //--------------------------------------------
-  const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch'
+  
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
-  },
-}));
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      [theme.breakpoints.up("sm")]: {
+        width: "15ch",
+      },
+    },
+  }));
 
   const DrawerList = (
     <Box
@@ -136,85 +135,111 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
           </ListItem>
         </Link>
         <Divider />
-        <Link to={"/CharacterDetails/1"} style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to={"/CharacterDetails/1"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <ListItem>
             <Face2Icon sx={{ paddingRight: "10px" }} /> Productos
           </ListItem>
         </Link>
         <Divider />
         <Divider />
-        <Link to={"https://github.com/alvarox86/Coffee-Express-Client"} style={{ textDecoration: "none", color: "black" }} target="_blank">
+        <Link
+          to={"https://github.com/alvarox86/Coffee-Express-Client"}
+          style={{ textDecoration: "none", color: "black" }}
+          target="_blank"
+        >
           <ListItem>
             <GitHubIcon sx={{ paddingRight: "10px" }} /> Repository
           </ListItem>
         </Link>
         <Divider />
-        <Link to={"/AboutPage"} style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to={"/AboutPage"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <ListItem>
             <InfoIcon sx={{ paddingRight: "10px" }} /> About the page
           </ListItem>
         </Link>
         <Divider />
-        
       </List>
     </Box>
   );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="inherit">
-        <Toolbar className="toolBar" sx={{ margin: "10px" }}>
+      <AppBar position="fixed" color="default">
+        <Toolbar
+          className="toolBar"
+          sx={{
+            minHeight: "60px",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2, xs: 'none', sm: 'block' }}
+            sx={{ mr: 2 }}
           >
             <MenuIcon onClick={toggleDrawer(true)} />
-            <Drawer
-              open={open}
-              onClose={toggleDrawer(false)}
-              sx={{ margin: "20px" }}
-            >
+            <Drawer open={open} onClose={toggleDrawer(false)}>
               {DrawerList}
             </Drawer>
           </IconButton>
+
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <img src={cafeicon} alt="LogoPagina" style={{ height: "80px", marginRight: "20px"}} />
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, textAlign: "center",display:"flex",flexDirection:"row" }}
+            
+            sx={{ fontWeight: "bold" }}
           >
-            <img src={cafeicon}alt="LogoPagina" style={{ height: "100px" }} />
             <h1>Coffee Express</h1>
           </Typography>
+          </Box>
+          
           <Search className="searchBtnNavBar">
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
-          <Link to={"/signup"}  style={{ textDecoration: "none", color: "black" }}>
+
+          {/* Auth */}
+          <Link
+            to={"/signup"}
+            style={{ textDecoration: "none", color: "black", marginLeft: "20px"}}
+          >
             {isLoggedIn === true ? (
-              <>
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
                 <div className="loggedCard">
-                  <img src={userProfilePicture} alt="User Icon" style={{width:"50px"}}/>
-                  <p>{userUserName}</p>
+                  <img src={userProfilePicture} alt="User" style={{ width: "36px", borderRadius: "50%" }} />
+                   <Typography variant="body1">{userUserName}</Typography>
                 </div>
-                <button onClick={handleLogout}>LogOut</button>
-              </>
-            ):(
+                 <button onClick={handleLogout} style={{ marginLeft: "10px" }}>LogOut</button>
+              </Box>
+            ) : (
               <>
-                <AccountCircleIcon sx={{width:"50px", height:"50px"}} />
+                <AccountCircleIcon sx={{ width: "50px", height: "50px" }} />
+                <Typography variant="body1">Sign Up</Typography>
               </>
-              )
-            }
+            )}
           </Link>
         </Toolbar>
       </AppBar>
+      
+      {/*Para que no tape el contenido porque el navbar está fixed */}
+      <Toolbar/>
+      <Toolbar/>
     </Box>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,150 @@
+import {
+  Box,
+  Typography,
+  Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Stack,
+  Container,
+} from "@mui/material";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import LocalCafeIcon from "@mui/icons-material/LocalCafe";
+
+/*Esto es para poner algunos cafes como destacados, debemos poner data de nuestro server, pero para que veas como quedaría */
+const featuredProducts = [
+  {
+    id: 1,
+    name: "Colombian Roast",
+    price: "12.99",
+    img: "/images/colombian.jpg",
+  },
+  {
+    id: 2,
+    name: "Ethiopian Blend",
+    price: "14.99",
+    img: "/images/ethiopian.jpg",
+  },
+  {
+    id: 3,
+    name: "House Espresso",
+    price: "11.50",
+    img: "/images/espresso.jpg",
+  },
+  { id: 4, name: "Decaf Delight", price: "13.25", img: "/images/decaf.jpg" },
+];
+
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <Box>
+      {/* Hero Section */}
+      {/*AÑADIR NUESTRA IMAGEN DE FONDO AQUÍ CON CLOUDINARY */}
+      <Box
+        sx={{
+          height: "90vh",
+          backgroundImage: "url(/images/hero-coffee.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "white",
+          px: 2,
+        }}
+      >
+        <Box sx={{ bgcolor: "rgba(0,0,0,0.5)", p: 4, borderRadius: 2 }}>
+          <Typography variant="h2" fontWeight="bold">
+            Coffee Express
+          </Typography>
+          <Typography variant="h5" sx={{ my: 2 }}>
+            Bold flavors. Fast delivery. Coffee that powers your day.
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ color: "white", backgroundColor: "#8B5042" }}
+            size="large"
+          >
+            Browse Our Coffees
+          </Button>
+        </Box>
+      </Box>
+
+      {/* Featured Products */}
+      <Container sx={{ py: 6 }}>
+        <Typography variant="h4" textAlign="center" fontWeight="bold" mb={4}>
+          Featured Coffees
+        </Typography>
+        <Grid container spacing={4} sx={{display: "flex", justifyContent: "center"}}>
+          {featuredProducts.map((product) => (
+            <Grid item xs={12} sm={6} md={3} key={product.id}>
+              <Card>
+                <CardMedia component="img" height="200" image={product.img} />
+                <CardContent>
+                  <Typography variant="h6">{product.name}</Typography>
+                  <Typography>{product.price} €</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" sx={{ color: "#8B5042" }}>
+                    View More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+
+      {/* Why Choose Us */}
+      <Box sx={{ backgroundColor: "#F2E8DF", py: 6, display: "flex", justifyContent: "center"}} >
+        <Container>
+          <Typography
+            variant="h4"
+            textAlign="center"
+            fontWeight="bold"
+            mb={4}
+            sx={{ color: "#592C27" }}
+          >
+            Why Choose Us
+          </Typography>
+          <Grid container spacing={4} sx={{display: "flex", justifyContent: "center"}}>
+            <Grid item xs={12} sm={6} md={3}>
+              <Stack alignItems="center" spacing={1}>
+                <CoffeeIcon fontSize="large" sx={{ color: "#D9A689" }} />
+                <Typography sx={{ color: "#261420" }}>
+                  Specialty Beans
+                </Typography>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Stack alignItems="center" spacing={1}>
+                <LocalShippingIcon fontSize="large" sx={{ color: "#D9A689" }} />
+                <Typography sx={{ color: "#261420" }}>24h Delivery</Typography>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Stack alignItems="center" spacing={1}>
+                <VerifiedIcon fontSize="large" sx={{ color: "#D9A689" }} />
+                <Typography sx={{ color: "#261420" }}>
+                  100% Guarantee
+                </Typography>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Stack alignItems="center" spacing={1}>
+                <LocalCafeIcon fontSize="large" sx={{ color: "#D9A689" }} />
+                <Typography sx={{ color: "#261420" }}>Eco-Friendly</Typography>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
+  );
 }
 
-export default HomePage
+export default HomePage;
